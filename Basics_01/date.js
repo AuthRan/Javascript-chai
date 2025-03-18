55c137541dd97664194a1acacd282c91aba48152
Ashutosh Ranjan <authran.off@gmail.com>
2025-03-18 14:15:41 +0000

    Basics_01 completed

---
+++ Basics_01/date.js
@@ -0,0 +1,10 @@
+// date
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleString())
+
