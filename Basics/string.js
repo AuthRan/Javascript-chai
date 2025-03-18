a6c3bb42b3f87841b816321674a0de0669a0ca26
Ashutosh Ranjan <authran.off@gmail.com>
2025-03-18 13:45:48 +0000

    changes in string

--- Basics/string.js
+++ Basics/string.js
@@ -1,3 +1,6 @@
-const sname = "ashutosh"
-const count = 1
-console.log(`Hello my name is ${sname} and my count is ${count} \nheheeee`);    //String Interpolation
+// const sname = "ashutosh"
+// const count = 1
+// console.log(`Hello my name is ${sname} and my count is ${count} \nheheeee`);    //String Interpolation
+
+let myName = new String('Ashutosh')
+console.log(myName.length);
