2b5d9813417111bf50c5c655cde24a6b229181bc
Ashutosh Ranjan <authran.off@gmail.com>
2025-03-23 17:38:58 +0000

    obj2

--- Basics_02/object.js
+++ Basics_02/object.js
@@ -1,4 +1,17 @@
-const obj1 {
-    name : "Ashutosh Ranjan"
-    G
+// const obj = {
+//     name : "ashutosh ranjan",
+//     roll_num : "B230006CS",
+//     cgpa : 7.01
+// };
+// console.log(obj.cgpa);
+// console.log(obj["name"]);
+
+
+let obj1 = {
+    name : "ashutosh",
+    roll : 6
+};
+
+for (let key in obj1) {
+    console.log(key +" : "+obj1[key]);
 }
